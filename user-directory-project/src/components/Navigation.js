70a124da7eb2c0327b4324e1625c9c03a4ea5aba
Joe Fatani <jucepig@gmail.com>
2021-04-22 19:28:42 -0600

    completed assignment

--- user-directory-project/src/components/Navigation.js
+++ user-directory-project/src/components/Navigation.js
@@ -1,15 +1,15 @@
-function Navigation () {
+function Navigation (props) {
   const prevBtn = "< Previous"
   const nextBtn = "Next >"
   return(
     <nav className="navigation flex-row">
-      <h4>{prevBtn}</h4>
+      <button disabled={props.index === 0} className="change-user-btn" onClick={() => props.decIndex()}>{prevBtn}</button>
       <div className="btns flex-row">
         <button className="cud-btn">Edit</button>
         <button className="cud-btn">Delete</button>
         <button className="cud-btn">New</button>
       </div>
-      <h4>{nextBtn}</h4>
+      <button disabled={props.index === 24} className="change-user-btn" onClick={() => props.incIndex()}>{nextBtn}</button>
     </nav>
   )
 }
--- user-directory-project/src/components/Slides.js
+++ user-directory-project/src/components/Slides.js
@@ -7,33 +7,83 @@ class Slides extends Component {
     super()
 
     this.state = {
-      users : Data
+      users : Data,
+      displayedUser: [{
+        id: 0,
+        name: { first: "", last: "" },
+        city: "",
+        country: "",
+        employer: "",
+        title: "",
+        favoriteMovies: [
+          "",
+          "",
+          ""
+        ]}],
+      index: 0
+    }
+    this.incIndex = this.incIndex.bind(this)
+    this.decIndex = this.decIndex.bind(this)
+  }
+
+  componentDidUpdate(pP, prevState) {
+    if(this.state.index !== prevState.index){
+      let users = this.state.users
+      let index = this.state.index
+      let selectedUser = users[index]
+      this.setState({
+        displayedUser: [selectedUser]
+      })
     }
   }
 
+  componentDidMount() {
+    let users = this.state.users
+    let index = this.state.index
+    let selectedUser = users[index]
+    this.setState({
+      displayedUser: [selectedUser]
+    })
+  }
+
+  incIndex() {
+    this.setState({
+      index: this.state.index +1
+    })
+    console.log(this.state.index)
+  }
+
+  decIndex() {
+    this.setState({
+      index: this.state.index -1
+    })
+    console.log(this.state.index)
+  }
+
   render() {
+    const selectedUser = this.state.displayedUser[0];
     return (
       <div className="user-container flex-row">
         <section className="user-directory">
-          <p className="counter">1/25</p>
-          <h1 className="name">FirstName LastName</h1>
+          <p className="counter">{this.state.index + 1}/25</p>
+          <h1 className="name">{selectedUser.name.first} {selectedUser.name.last}</h1>
           <div id="user-info-1">
-            <p><strong>From:</strong> blah blah blah</p>
-            <p><strong>Job Title:</strong> blah blah blah</p>
-            <p><strong>Employer:</strong> blah blah blah</p>
+            <p><strong>From: </strong>{selectedUser.city}, {selectedUser.country}</p>
+            <p><strong>Job Title: </strong>{selectedUser.title}</p>
+            <p><strong>Employer: </strong>{selectedUser.employer}</p>
           </div>
           
           <div id="user-info-2">
             <p><strong>Favorite Movies:</strong></p>
               <ol>
-                <li>first</li>
-                <li>second</li>
-                <li>third</li>
+                <li>{selectedUser.favoriteMovies[0]}</li>
+                <li>{selectedUser.favoriteMovies[1]}</li>
+                <li>{selectedUser.favoriteMovies[2]}</li>
               </ol>
           </div>
         </section>
 
-        <Navigation/>
+        <Navigation index={this.state.index} incIndex={this.incIndex} decIndex={this.decIndex}/>
 
       </div>
     )
